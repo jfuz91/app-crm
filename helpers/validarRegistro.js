ac3200c755a5301ffd3a0192e74e18a57c0ac38c
jfuz91 <felipeuz.07@gmail.com>
2025-09-13 14:56:45 -0500

    cambios en validacion

---
+++ helpers/validarRegistro.js
@@ -0,0 +1,9 @@
+let input = document.querySelectorAll("form div input")
+let arreloinput =[...input]
+console.log (inputs)
+console.log (arregloinput)
+
+arregloinput.map((input) =>{
+console.log (input);
+})
+
